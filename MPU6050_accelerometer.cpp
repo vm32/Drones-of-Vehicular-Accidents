636fe6882be0481e89a961ccf4e2956fc28fab5c
Abdullah <21219411+iiAbdullah1337@users.noreply.github.com>
2023-01-12 18:24:01 +0300

    update the code #1

--- MPU6050_accelerometer.cpp
+++ MPU6050_accelerometer.cpp
@@ -1,45 +1,59 @@
-#include "Wire.h" 
-const int MPU_ADDR = 0x68; 
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
 
-int16_t accelerometer_x, accelerometer_y, accelerometer_z; 
-int16_t gyro_x, gyro_y, gyro_z; 
-int16_t temperature; 
+Adafruit_MPU6050 mpu;
 
-char tmp_str[7]; 
+void setup(void) {
+	Serial.begin(115200);
 
-char* convert_int16_to_str(int16_t i) { 
-  sprintf(tmp_str, "%6d", i);
-  return tmp_str;
+	// Try to initialize!
+	if (!mpu.begin()) {
+		Serial.println("Failed to find MPU6050 chip");
+		while (1) {
+		  delay(10);
+		}
+	}
+	Serial.println("MPU6050 Found!");
+
+	// set accelerometer range to +-8G
+	mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+
+	// set gyro range to +- 500 deg/s
+	mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+
+	// set filter bandwidth to 21 Hz
+	mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+
+	delay(100);
 }
 
-void setup() {
-  Serial.begin(9600);
-  Wire.begin();
-  Wire.beginTransmission(MPU_ADDR); 
-  Wire.write(0x6B); r
-  Wire.write(0); 
-  Wire.endTransmission(true);
-}
 void loop() {
-  Wire.beginTransmission(MPU_ADDR);
-  Wire.write(0x3B); 
-  Wire.requestFrom(MPU_ADDR, 7*2, true); 
-  
-  accelerometer_x = Wire.read()<<8 | Wire.read(); 
-  accelerometer_y = Wire.read()<<8 | Wire.read(); 
-  accelerometer_z = Wire.read()<<8 | Wire.read(); 
-  temperature = Wire.read()<<8 | Wire.read(); 
-  gyro_x = Wire.read()<<8 | Wire.read(); 
-  gyro_y = Wire.read()<<8 | Wire.read(); 
-  gyro_z = Wire.read()<<8 | Wire.read(); 
-  Serial.print("aX = "); Serial.print(convert_int16_to_str(accelerometer_x));
-  Serial.print(" | aY = "); Serial.print(convert_int16_to_str(accelerometer_y));
-  Serial.print(" | aZ = "); Serial.print(convert_int16_to_str(accelerometer_z));
-  Serial.print(" | tmp = "); Serial.print(temperature/340.00+36.53);
-  Serial.print(" | gX = "); Serial.print(convert_int16_to_str(gyro_x));
-  Serial.print(" | gY = "); Serial.print(convert_int16_to_str(gyro_y));
-  Serial.print(" | gZ = "); Serial.print(convert_int16_to_str(gyro_z));
-  Serial.println();
-  
-  delay(1000);
+	/* Get new sensor events with the readings */
+	sensors_event_t a, g, temp;
+	mpu.getEvent(&a, &g, &temp);
+
+	/* Print out the values */
+	Serial.print("Acceleration X: ");
+	Serial.print(a.acceleration.x);
+	Serial.print(", Y: ");
+	Serial.print(a.acceleration.y);
+	Serial.print(", Z: ");
+	Serial.print(a.acceleration.z);
+	Serial.println(" m/s^2");
+
+	Serial.print("Rotation X: ");
+	Serial.print(g.gyro.x);
+	Serial.print(", Y: ");
+	Serial.print(g.gyro.y);
+	Serial.print(", Z: ");
+	Serial.print(g.gyro.z);
+	Serial.println(" rad/s");
+
+	Serial.print("Temperature: ");
+	Serial.print(temp.temperature);
+	Serial.println(" degC");
+
+	Serial.println("");
+	delay(500);
 }
